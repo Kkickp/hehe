d3eb4da80dc89cb0f7f2bfacf6fa2ffa4fffdb63
Kkickp <146531388+Kkickp@users.noreply.github.com>
2024-08-04 16:50:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Avvv, i love u moree";
+  question.innerHTML = "senang mendengarnya, semoga selalu bahagia ya";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
